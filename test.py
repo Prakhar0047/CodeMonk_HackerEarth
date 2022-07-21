3bdee4a99ad65d3a69481f10cd5f539eace3a3a4
Prakhar <prakharv047@gmail.com>
2022-07-21 13:41:57 +0530

    Codes

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-j = [0]*10
-print(j)
+def longestEvenWord(s):
+  ll = s.split()
+  lon = ""
+  for x in ll:
+    if len(x)%2==0 and len(x) > len(lon):
+      lon = x
+  return lon
+
+sentence = 'Time to write great code'
+ans = longestEvenWord(sentence)
+print(ans)
